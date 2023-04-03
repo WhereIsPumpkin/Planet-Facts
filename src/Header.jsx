21920d7f157e39315ad5091598f526782b081cd7
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-04-03 15:27:47 +0400

    tablet design

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,57 +1,69 @@
-import React, { useState } from "react";import data from "./data.json";
-import styles from "./header.module.scss"
-import arrow from "./assets/icon-chevron.svg"
+import React, { useState } from "react";
+import data from "./data.json";
+import styles from "./header.module.scss";
+import arrow from "./assets/icon-chevron.svg";
 import { Link, Route, Routes } from "react-router-dom";
 
-
-
 export default function Header() {
-
   const [showMenu, setShowMenu] = useState(false);
-  
+
   const planetColors = {
-    mercury: '#DEF4FC',
-    venus: '#F7CC7F',
-    earth: '#545BFE',
-    mars: '#FF6A45',
-    jupiter: '#ECAD7A',
-    saturn: '#FCCB6B',
-    uranus: '#65F0D5',
-    neptune: '#497EFA'
+    mercury: "#DEF4FC",
+    venus: "#F7CC7F",
+    earth: "#545BFE",
+    mars: "#FF6A45",
+    jupiter: "#ECAD7A",
+    saturn: "#FCCB6B",
+    uranus: "#65F0D5",
+    neptune: "#497EFA",
   };
 
   return (
     <>
-
       <header>
         <h1>THE PLANETS</h1>
 
-        <div className={styles.menuIcon} onClick={() => setShowMenu(!showMenu)} style={{ opacity: showMenu ? 0.2 : 1 }}>
+        <div
+          className={styles.menuIcon}
+          onClick={() => setShowMenu(!showMenu)}
+          style={{ opacity: showMenu ? 0.2 : 1 }}
+        >
           <div className={styles.line}></div>
           <div className={styles.line}></div>
           <div className={styles.line}></div>
         </div>
-        
       </header>
 
       {showMenu && (
-        <ul className={styles.menuPop} >
-        {data.map((planet) => (
-          <li key={planet.name}
-          onClick={() => setShowMenu(!showMenu)} >
-            <Link to={`/planets/${planet.name.toLowerCase()}/overview`}>
-              <div className={styles.wrapperLi}>
-                <div className={styles.circle} style={{ backgroundColor: planetColors[planet.name.toLowerCase()] }}></div>
-                {planet.name}
-              </div>
-              <img src={arrow} alt="arrowChevron" />
-            </Link>
-          </li>
-        ))}
-      </ul>
+        <ul className={styles.menuPop}>
+          {data.map((planet) => (
+            <li key={planet.name} onClick={() => setShowMenu(!showMenu)}>
+              <Link to={`/planets/${planet.name.toLowerCase()}/overview`}>
+                <div className={styles.wrapperLi}>
+                  <div
+                    className={styles.circle}
+                    style={{
+                      backgroundColor: planetColors[planet.name.toLowerCase()],
+                    }}
+                  ></div>
+                  {planet.name}
+                </div>
+                <img src={arrow} alt="arrowChevron" />
+              </Link>
+            </li>
+          ))}
+        </ul>
       )}
 
+      <div className={styles.planetList}>
+        {data.map((planet) => (
+          <div key={planet.name} className={styles.planetBlock}>
+            <Link to={`/planets/${planet.name.toLowerCase()}/overview`}>
+              <span>{planet.name}</span>
+            </Link>
+          </div>
+        ))}
+      </div>
     </>
   );
-
 }
--- src/pages/Planet.jsx
+++ src/pages/Planet.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import data from "../data.json";
 import styles from "./Planet.module.scss";
@@ -10,8 +10,11 @@ export default function Planet() {
     (planet) => planet.name.toLowerCase() === name.toLowerCase()
   );
 
+  const [activeLink,setActiveLink] = useState('');
+
   console.log(planetData.images.planet);
   let content;
+
   switch (menu) {
     case "overview":
       content = planetData?.overview?.content;
@@ -37,6 +40,7 @@ export default function Planet() {
         >
           OVERVIEW
         </Link>
+        
         <Link
           to={`/planets/${name}/structure`}
           className={`${styles.menuElement} ${
@@ -59,56 +63,97 @@ export default function Planet() {
         {planetData ? (
           <div className={styles.infoSection}>
             <div className={styles.imgWrapper}>
-            <img
-              src={
-                menu === "overview"
-                  ? planetData.images.planet
-                  : menu === "structure"
-                  ? planetData.images.internal
-                  : menu === "surface"
-                  ? planetData.images.planet
-                  : null
-              }
-              alt={`${planetData.name} planet`}
-            />
-             {menu === "surface" && (
-          <img
-            src={planetData.images.geology}
-            alt={`${planetData.name} geology`}
-            className={styles.geology}
-          />
-        )}
+              <img
+                src={
+                  menu === "overview"
+                    ? planetData.images.planet
+                    : menu === "structure"
+                    ? planetData.images.internal
+                    : menu === "surface"
+                    ? planetData.images.planet
+                    : null
+                }
+                alt={`${planetData.name} planet`}
+              />
+              {menu === "surface" && (
+                <img
+                  src={planetData.images.geology}
+                  alt={`${planetData.name} geology`}
+                  className={styles.geology}
+                />
+              )}
             </div>
 
-            <div className={styles.nameDescription}>
-              <h1 className={styles.name}>{planetData.name}</h1>
-              <p className={styles.description}>{content}</p>
-              <span>
-                Source:{" "}
-                <a
-                  href={
-                    menu === "surface"
-                      ? planetData["geology"]?.source
-                      : planetData[menu]?.source
-                  }
-                >
-                  Wikipedia{" "}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
+            <div className={styles.infoSectionWrapper}>
+              <div className={styles.nameDescription}>
+                <h1 className={styles.name}>{planetData.name}</h1>
+                <p className={styles.description}>{content}</p>
+                <span>
+                  Source:{" "}
+                  <a
+                    href={
+                      menu === "surface"
+                        ? planetData["geology"]?.source
+                        : planetData[menu]?.source
+                    }
+                    target="_blank"
                   >
-                    <path
-                      fill="#FFF"
-                      d="M11.34.66C10.9.22 10.37 0 9.75 0h-7.5C1.63 0 1.1.22.66.66.22 1.1 0 1.63 0 2.25v7.5c0 .62.22 1.15.66 1.59.44.44.97.66 1.59.66h7.5c.62 0 1.15-.22 1.59-.66.44-.44.66-.97.66-1.59v-7.5c0-.62-.22-1.15-.66-1.59zM10 6.25a.467.467 0 01-.305.46.544.544 0 01-.195.04.465.465 0 01-.352-.149L8.023 5.476 3.852 9.648a.481.481 0 01-.352.149.48.48 0 01-.352-.149l-.796-.797a.48.48 0 01-.149-.351.48.48 0 01.149-.352l4.172-4.172-1.125-1.125c-.162-.15-.198-.333-.11-.546A.467.467 0 015.75 2H9.5c.135 0 .253.05.352.148A.48.48 0 0110 2.5v3.75z"
-                      opacity=".5"
-                    />
-                  </svg>
-                </a>
-              </span>
+                    Wikipedia{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="12"
+                      height="12"
+                    >
+                      <path
+                        fill="#FFF"
+                        d="M11.34.66C10.9.22 10.37 0 9.75 0h-7.5C1.63 0 1.1.22.66.66.22 1.1 0 1.63 0 2.25v7.5c0 .62.22 1.15.66 1.59.44.44.97.66 1.59.66h7.5c.62 0 1.15-.22 1.59-.66.44-.44.66-.97.66-1.59v-7.5c0-.62-.22-1.15-.66-1.59zM10 6.25a.467.467 0 01-.305.46.544.544 0 01-.195.04.465.465 0 01-.352-.149L8.023 5.476 3.852 9.648a.481.481 0 01-.352.149.48.48 0 01-.352-.149l-.796-.797a.48.48 0 01-.149-.351.48.48 0 01.149-.352l4.172-4.172-1.125-1.125c-.162-.15-.198-.333-.11-.546A.467.467 0 015.75 2H9.5c.135 0 .253.05.352.148A.48.48 0 0110 2.5v3.75z"
+                        opacity=".5"
+                      />
+                    </svg>
+                  </a>
+                </span>
+              </div>
+
+              <div className={styles.tabletMenu}>
+
+                <Link
+                  to={`/planets/${name}/overview`}
+                  className={`${styles.menuElement} ${
+                    activeLink === "overview" ? styles.active : null
+                  }`}
+                  onClick={() => setActiveLink("overview")}
+                >
+                  <span>01</span>
+                  OVERVIEW
+                </Link>
+
+                <Link
+                  to={`/planets/${name}/structure`}
+                  className={`${styles.menuElement} ${
+                    activeLink === "structure" ? styles.active : null
+                  }`}
+                  onClick={() => setActiveLink("structure")}
+                >
+                  <span>02</span>
+                  INTERNNAL STRUCTURE
+                </Link>
+
+                <Link
+                  to={`/planets/${name}/surface`}
+                  className={`${styles.menuElement} ${
+                    activeLink === "surface" ? styles.active : null
+                  }`}
+                  onClick={() => setActiveLink("surface")}
+                >
+                  <span>03</span>
+                  SURFACE GEOLOGOY
+                </Link>
+
+              </div>
             </div>
 
             <div className={styles.basicInfo}>
+
               <div className={styles.Component}>
                 <h2>ROTATION TIME</h2>
                 <span>{planetData.rotation}</span>
@@ -130,6 +175,7 @@ export default function Planet() {
               </div>
 
             </div>
+            
           </div>
         ) : (
           <p>Planet not found</p>
